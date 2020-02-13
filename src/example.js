9481ee5927c443b144c6d28ea347a9a7e0a63f22
Urx2RHm6 <russell@unseelieventures.com>
2020-02-12 20:36:12 -0600

    Initial commit

---
+++ src/example.js
@@ -0,0 +1,378 @@
+import React from "react";
+import { Grid, Input, Header, Divider } from "semantic-ui-react";
+
+const GridExampleCelled = () => (
+  // If you touch anything above this line you will break it!
+  <Grid celled>
+    <Grid.Row>
+      <Grid.Column width={5}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="DATE" />
+</div>
+      </Grid.Column>
+      <Grid.Column width={8}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="COMPANY" />
+</div>
+</Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column width={8}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="LEASE NAME" />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="WELL #" />
+</div>
+</Grid.Column>
+      
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={5}>
+        <div class="ui fluid icon input">
+  <input type="text" placeholder="RRC#" />
+</div>
+</Grid.Column>
+      <Grid.Column width={5}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="STATE" />
+</div>
+</Grid.Column>
+      <Grid.Column width={5}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="COUNTY" />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={5}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="DISPOSAL" />
+</div>
+</Grid.Column>
+      <Grid.Column width={5}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="TRUCK#" />
+</div>
+</Grid.Column>
+      <Grid.Column width={5}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="HRS" />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8}>
+      <Header size='large' textAlign='center'>Gauges</Header>
+      </Grid.Column>
+      <Grid.Column width={3}>
+      <Header size='large' textAlign='center'> TANK# </Header>
+      </Grid.Column>
+      
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="T '" />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="T" />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="" />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="B '" />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="B " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="Bbls" />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="KK '" />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="KK " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="" />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <div class="ui divider"></div>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="T ' " />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="T " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="B '" />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="B " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="Bbls " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="KK ' " />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="KK " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <div class="ui divider"></div>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="T '" />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="T " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="B ' " />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="B  " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="Bbls " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="KK ' " />
+</div>
+</Grid.Column>
+      <Grid.Column width={4}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="KK " />
+</div>
+</Grid.Column>
+      <Grid.Column width={6}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <div class="ui divider"></div>
+    <Grid.Row>
+      <Grid.Column width={9}>
+      <Header size='small' textAlign='center'> Description of Work Performed </Header>
+      </Grid.Column>
+      <Grid.Column width={3}>
+      <Header size='small' textAlign='center'> Rate </Header>
+      </Grid.Column>
+      <Grid.Column width={3}>
+      <Header size='small' textAlign='center'> Amount </Header>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={9}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="Trucking " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={9}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder="Disposal Fee/bbl " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={9}>
+        <div class="ui fluid icon input">
+  <input type="text" placeholder="Fresh Water Fee/bbl " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={9}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" KCI/Chemical Fee/bbl" />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={9}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" Other Fee" />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+    </Grid.Row>
+    <Grid.Row>
+    <Grid.Column width={12}>
+    <Header size='small' textAlign='right'> Sales Tax </Header>
+    </Grid.Column>
+    <Grid.Column width={3}>
+    <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+      <Grid.Column width={12}>
+      <Header size='small' textAlign='right'> Total </Header>  
+      </Grid.Column>
+      <Grid.Column width={3}>
+      <div class="ui fluid icon input">
+  <input type="text" placeholder=" " />
+</div>
+</Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={10}>
+        <div class="ui fluid icon input">
+  <input type="text" placeholder="Work Ordered By " />
+</div>
+</Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+        <Grid.Column width={10}>
+          <div class="ui fluid icon input">
+  <input type="text" placeholder="Approved By " />
+</div>
+</Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+        <Grid.Column width={10}>
+        <div class="ui fluid icon input">
+  <input type="text" placeholder="Driver " />
+</div>
+</Grid.Column>
+        </Grid.Row>
+
+
+   
+  </Grid>
+ 
+
+  // If you touch anything below this line you will break it!
+);
+
+export default GridExampleCelled;
